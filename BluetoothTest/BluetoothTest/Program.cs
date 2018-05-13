5425c497b2d4dbd2cebc07b89b18604755916da9
Hyo-Seong <dlgytjd0907@naver.com>
2018-05-13 19:47:42 +0900

    c# bluetooth 통신
    테스트코드.
    이 코드 기반으로 나르샤 코드 개발 진행/

---
+++ BluetoothTest/BluetoothTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BluetoothTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serialPort = new SerialPort();
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM8"; // 송신
+            serialPort.Open();
+            int counter = 0;
+            while (serialPort.IsOpen)
+            {
+                // WRITE THE INCOMING BUFFER TO CONSOLE
+                while (serialPort.BytesToRead > 0)
+                {
+                    Console.Write(Convert.ToChar(serialPort.ReadChar()));
+                }
+                // SEND
+                serialPort.Write("aa");
+                Console.WriteLine("PC counter: " + (counter++));
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
