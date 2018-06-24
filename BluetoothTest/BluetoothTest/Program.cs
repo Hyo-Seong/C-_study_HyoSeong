a97b6a6894be213e9ba29ac01196f8da217aafec
Hyo-Seong <dlgytjd0907@naver.com>
2018-06-24 22:12:45 +0900

    오랜만에c#공부,,?

--- BluetoothTest/BluetoothTest/Program.cs
+++ BluetoothTest/BluetoothTest/Program.cs
@@ -21,18 +21,22 @@ namespace BluetoothTest
             Console.WriteLine("Connected!");
             serialPort.Write("Connected!");
             String response = null;
+            int i = 0;
             while (serialPort.IsOpen)
             {
                 response = null;
                 // WRITE THE INCOMING BUFFER TO CONSOLE
+                //Console.Write(Convert.ToChar(serialPort.ReadChar()));
+                //Console.WriteLine(Convert.ToChar(serialPort.ReadChar()));
                 while (serialPort.BytesToRead > 0)
                 {
-                    
+                    Console.Write("1");
                     response += Convert.ToChar(serialPort.ReadChar());
                 }
                 // SEND
                 Console.WriteLine(response);
-                serialPort.Write("ddaa + " + response);
+                i++;
+                serialPort.WriteLine("a + " + i);
                 Thread.Sleep(500);
             }
         }
